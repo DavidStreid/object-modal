f799760e7772fa5030e152a717594ce0f21c90be
David Streid <david.streid.nyc@gmail.com>
2020-06-28 07:13:43 -0400

    do-nothing test

--- src/modal-container.test.js
+++ src/modal-container.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './modal-container';
+import { render, screen } from '@testing-library/react';
+import ModalContainer from './modal-container';
+import {Subject} from "rxjs";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async () => {
+  const subj = new Subject();
+  const { container, debug } = render(<ModalContainer modalUpdater={subj}/>);
+  subj.next({msg: 'hey', type: 'MODAL_ERROR', delay: 1000});
+  debug();
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
